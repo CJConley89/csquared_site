1449da02c62c506bbc16b1521bddcd6125833b80
CJConley89 <142371380+CJConley89@users.noreply.github.com>
2023-08-27 08:59:15 -0500

    added logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BsChatSquareDots } from 'react-icons/bs'
+import logoIMG from '../assets/logo.png'
 
 const Footer = () => {
   return (
     <div className='max-w-[1140px] w-full py-8 m-auto border-t-4'>
         <div className='flex items-center m-auto justify-center'>
-            <BsChatSquareDots size={30} className='text-[var(--primary-dark)] mr-2'/>
+          <img src={logoIMG} alt="logo" width='80' height='80' className='mr-2'/>
             <h1 className='text-xl font-bold text-gray-700'>C2 Woodworking</h1>
         </div>
     </div>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai'
-import {BsChatSquareDots} from 'react-icons/bs'
+import logoIMG from '../assets/logo.png'
 
 const TopBar = () => {
   return (
     <div className='flex justify-between items-center px-4 py-2'>
         <div className='flex items-center'>
-            <BsChatSquareDots size='30' className='text-[var(--primary-dark)] mr-2'/>
+            <img src={logoIMG} alt="logo" width='80' height='80'/>
             <h1 className='text-xl font-bold text-gray-700'>C2 Woodworking</h1>
         </div>
         <div className='flex'>
